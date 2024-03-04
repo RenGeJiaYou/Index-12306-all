8e3c260e034fb8255bff95b45fa05c39c3b1664b
Shi Jiajun <1178972961@qq.com>
2024-03-04 17:59:29 +0800

    feature：开始幂等组件库 - Token 方式

--- frameworks/idempotent/src/main/java/com/sjj/idempotentspringbootstarter/config/IdempotentAutoConfiguration.java
+++ frameworks/idempotent/src/main/java/com/sjj/idempotentspringbootstarter/config/IdempotentAutoConfiguration.java
@@ -1,10 +1,12 @@
 package com.sjj.idempotentspringbootstarter.config;
 
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
 /**
  * 消息队列幂等自动配置类
  *
  * @author Island_World
  */
-
+@EnableConfigurationProperties(IdempotentProperties.class)
 public class IdempotentAutoConfiguration {
 }
--- frameworks/idempotent/src/main/java/com/sjj/idempotentspringbootstarter/core/token/IdempotentTokenExecuteHandler.java
+++ frameworks/idempotent/src/main/java/com/sjj/idempotentspringbootstarter/core/token/IdempotentTokenExecuteHandler.java
@@ -1,13 +1,21 @@
 package com.sjj.idempotentspringbootstarter.core.token;
 
+import cn.hutool.core.util.StrUtil;
+import com.google.common.base.Strings;
 import com.sjj.cachespringbootstarter.DistributedCache;
+import com.sjj.conventionspringbootstarter.errorcode.BaseErrorCode;
 import com.sjj.conventionspringbootstarter.exception.ClientException;
+import com.sjj.idempotentspringbootstarter.config.IdempotentProperties;
 import com.sjj.idempotentspringbootstarter.core.AbstractIdempotentExecuteHandler;
-import com.sjj.idempotentspringbootstarter.core.IdempotentContext;
 import com.sjj.idempotentspringbootstarter.core.IdempotentParamWrapper;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.aspectj.lang.ProceedingJoinPoint;
-import org.redisson.api.RLock;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import java.util.Optional;
+import java.util.UUID;
 
 /**
  * 基于 Token 验证幂等性,通常应用于 RestAPI 方法
@@ -19,34 +27,53 @@ import org.redisson.api.RLock;
 @RequiredArgsConstructor
 public class IdempotentTokenExecuteHandler extends AbstractIdempotentExecuteHandler implements IdempotentTokenService {
     private final DistributedCache distributedCache;
+    private final IdempotentProperties idempotentProperties;
 
-
-
-
-
-
-
+    public static final String TOKEN_KEY = "token";
+    public static final String TOKEN_PREFIX_KEY = "idempotent:token:";
+    public static final long TOKEN_EXPIRED_TIME = 6000;
 
     /**
      * @Return 构造幂等验证过程中所需要的参数包装器
-     * */
+     */
     @Override
     public IdempotentParamWrapper buildWrapper(ProceedingJoinPoint joinPoint) {
-
+        return new IdempotentParamWrapper().setJoinPoint(joinPoint);
 
     }
 
     /**
      * handler() 中加可重入锁后，将锁放入上下文
-     * */
+     */
     @Override
     public void handler(IdempotentParamWrapper wrapper) {
-
+        HttpServletRequest request = ((ServletRequestAttributes) (RequestContextHolder.currentRequestAttributes())).getRequest();
+        String token = request.getHeader(TOKEN_KEY);
+        if (StrUtil.isBlank(token)) {
+            token = request.getParameter(TOKEN_KEY);
+            if (StrUtil.isBlank(token)) {
+                throw new ClientException(BaseErrorCode.IDEMPOTENT_TOKEN_NULL_ERROR);
+            }
+        }
+        Boolean deleteFlag = distributedCache.delete(token);
+        if (!deleteFlag) {
+            String ErrorMsg = StrUtil.isNotBlank(wrapper.getIdempotent().message())
+                    ? wrapper.getIdempotent().message()
+                    : BaseErrorCode.IDEMPOTENT_TOKEN_DELETE_ERROR.message();
+            throw new ClientException(ErrorMsg, BaseErrorCode.IDEMPOTENT_TOKEN_DELETE_ERROR);
+        }
     }
 
-
+    /**
+     * 先根据配置文件中的前缀和过期时间创建 token，没有则采用默认值
+     * 将 token 存到 redis 中并返回
+     *
+     * @return
+     */
     @Override
     public String createToken() {
-        return null;
+        String token = Optional.ofNullable(Strings.emptyToNull(idempotentProperties.getPrefix())).orElse(TOKEN_PREFIX_KEY) + UUID.randomUUID();
+        distributedCache.put(token, "", Optional.ofNullable(idempotentProperties.getTimeout()).orElse(TOKEN_EXPIRED_TIME));
+        return token;
     }
 }
