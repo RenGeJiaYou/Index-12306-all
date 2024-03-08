f5ea50728e9fcba7f63b2f1c7d204bc817336026
Shi Jiajun <37719543+RenGeJiaYou@users.noreply.github.com>
2024-03-08 17:08:42 +0800

    feature：完成全局异常类

--- frameworks/web/src/main/java/com/sjj/webspringbootstarter/GlobalExceptionHandler.java
+++ frameworks/web/src/main/java/com/sjj/webspringbootstarter/GlobalExceptionHandler.java
@@ -1,10 +1,81 @@
 package com.sjj.webspringbootstarter;
 
+import cn.hutool.core.collection.CollectionUtil;
+import cn.hutool.core.util.StrUtil;
+import com.sjj.conventionspringbootstarter.errorcode.BaseErrorCode;
+import com.sjj.conventionspringbootstarter.exception.AbstractException;
+import com.sjj.conventionspringbootstarter.result.Result;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Optional;
+
 /**
- * 全局异常处理
+ * 使用 @RestControllerAdvice 的就是全局异常处理类
  *
  * @author Island_World
  */
-
+@Slf4j
+@RestControllerAdvice
 public class GlobalExceptionHandler {
+    private String getUrl(HttpServletRequest request){
+        if (StrUtil.isEmpty(request.getQueryString())){
+            return request.getRequestURL().toString();
+        }
+        return request.getRequestURL() + "?" + request.getQueryString();
+    }
+
+    /**
+     * 拦截参数验证异常
+     *
+     * @param request
+     * @param ex
+     * @return 带有错误码和校验失败消息的失败响应 Result 对象
+     */
+    @SneakyThrows
+    @ExceptionHandler(value = MethodArgumentNotValidException.class)
+    public Result validExceptionHandler(HttpServletRequest request, MethodArgumentNotValidException ex){
+        // 包含了所有的验证错误信息
+        BindingResult bindingResult = ex.getBindingResult();
+        FieldError firstFieldError = CollectionUtil.getFirst(bindingResult.getFieldErrors());
+        String errMsg = Optional.ofNullable(firstFieldError)
+                .map(FieldError::getDefaultMessage)
+                .orElse(StrUtil.EMPTY);
+
+        log.error("[{}] {} [Valid Exception] {}",
+                request.getMethod(),
+                getUrl(request),
+                errMsg);
+
+        return Results.failure(BaseErrorCode.CLIENT_ERROR.code(), errMsg);
+    }
+
+    @ExceptionHandler(value = AbstractException.class)
+    public Result abstractException(HttpServletRequest request, AbstractException ex){
+        if(ex.getCause()!= null){
+            log.error("[{}] {} [Abstract Exception] {}",
+                    request.getMethod(),
+                    request.getRequestURL().toString(),
+                    ex,
+                    ex.getCause());
+        }
+        log.error("[{}] {} [ex] {}",
+                request.getMethod(),
+                request.getRequestURL().toString(),
+                ex.toString());
+        return Results.failure(ex);
+    }
+
+    @ExceptionHandler(value = Throwable.class)
+    public Result defaultErrorHandler(HttpServletRequest request,Throwable throwable){
+        log.error("[{}] {} ", request.getMethod(), getUrl(request), throwable);
+        return Results.failure();
+    }
+
 }
--- frameworks/web/src/main/java/com/sjj/webspringbootstarter/Results.java
+++ frameworks/web/src/main/java/com/sjj/webspringbootstarter/Results.java
@@ -19,17 +19,18 @@ public class Results {
      *
      * @return Result<Void>
      */
-    public static Result<Void> success(){
+    public static Result<Void> success() {
         // 将类的静态字段值赋给类的成员变量
         return new Result<Void>().setCode(Result.SUCCESS_CODE);
     }
 
     /**
      * 构造带数据的成功响应
+     *
      * @param data
      * @return Result<T>
      */
-    public static <T> Result<T> success(T data){
+    public static <T> Result<T> success(T data) {
         return new Result<T>().setCode(Result.SUCCESS_CODE).setData(data);
     }
 
@@ -38,7 +39,7 @@ public class Results {
      *
      * @return Result<void>
      */
-    protected static Result<Void> failure(){
+    protected static Result<Void> failure() {
         return new Result<Void>()
                 .setCode(BaseErrorCode.SERVICE_ERROR.code())
                 .setMessage(BaseErrorCode.SERVICE_ERROR.message());
@@ -50,10 +51,12 @@ public class Results {
      * @param abstractException
      * @return
      */
-    protected static Result<Void> failure(AbstractException abstractException){
-        String errorCode = Optional.ofNullable(abstractException.getErrorCode())
+    protected static Result<Void> failure(AbstractException abstractException) {
+        String errorCode = Optional
+                .ofNullable(abstractException.getErrorCode())
                 .orElse(BaseErrorCode.SERVICE_ERROR.code());
-        String errorMessage = Optional.ofNullable(abstractException.getErrorMessage())
+        String errorMessage = Optional
+                .ofNullable(abstractException.getErrorMessage())
                 .orElse(BaseErrorCode.SERVICE_ERROR.code());
 
         return new Result<Void>()
@@ -68,7 +71,7 @@ public class Results {
      * @param errorMessage
      * @return
      */
-    protected static Result<Void> failure(String errorCode,String errorMessage){
+    protected static Result<Void> failure(String errorCode, String errorMessage) {
         return new Result<Void>()
                 .setCode(errorCode)
                 .setMessage(errorMessage);
