b2088e95cbe40fba131a4637d62bd7acbc3f33ef
Shi Jiajun <37719543+RenGeJiaYou@users.noreply.github.com>
2024-03-09 16:12:50 +0800

    fix：对每个组件 Starter 查漏补缺

--- frameworks/bizs/user/src/main/java/com/sjj/userspringbootstarter/config/UserAutoConfiguration.java
+++ frameworks/bizs/user/src/main/java/com/sjj/userspringbootstarter/config/UserAutoConfiguration.java
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.sjj.userspringbootstarter.config;
 
 /*
@@ -6,8 +23,23 @@ package com.sjj.userspringbootstarter.config;
  * @author Island_World
  */
 
+import com.sjj.userspringbootstarter.core.UserTransmitFilter;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+
+import static com.sjj.basespringbootstarter.constant.FilterOrderConstant.USER_TRANSMIT_FILTER_ORDER;
 
 @ConditionalOnWebApplication
 public class UserAutoConfiguration {
+
+    @Bean
+    public FilterRegistrationBean<UserTransmitFilter> globalUserTransmitFilter(){
+        FilterRegistrationBean<UserTransmitFilter> registration = new FilterRegistrationBean<>();
+        registration.setFilter(new UserTransmitFilter());
+        registration.addUrlPatterns("/*");
+        registration.setOrder(USER_TRANSMIT_FILTER_ORDER);
+        return registration;
+    }
+
 }
