0c3b4884179c0dd1795a36cbaca3cdc521912499
Shi Jiajun <1178972961@qq.com>
2024-03-25 17:52:57 +0800

    feature：开发购票服务模块
    1. 增设一些配置文件，为 Nacos 和 Feign 做准备

--- services/ticket-service/src/main/java/com/sjj/ticketservice/TicketServiceApplication.java
+++ services/ticket-service/src/main/java/com/sjj/ticketservice/TicketServiceApplication.java
@@ -3,9 +3,11 @@ package com.sjj.ticketservice;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @MapperScan("com.sjj.ticketservice.dao.mapper")
+@EnableFeignClients()
 public class TicketServiceApplication {
 
     public static void main(String[] args) {
